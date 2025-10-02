adde9210ed021b9037a95d9e0f7ed018c3d22653
Andrew Reddin <areddin409@gmail.com>
2025-10-02 15:19:28 -0500

    refactor: restructure project layout and update dependencies

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-5xl text-red-300 underline">Hello World</h1>
+      <Button>Hello World</Button>
+    </div>
+  )
+}
+
+export default Home

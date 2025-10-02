f9bb399894d354f71efb44db5e5485b2322ac7b8
Andrew Reddin <areddin409@gmail.com>
2025-10-02 15:48:23 -0500

    feat: add user interface components and layout for stock tracking application
    - Implemented RootLayout component for application structure
    - Created Header component with navigation and user dropdown
    - Added NavItems component for navigation links
    - Developed UserDropdown component for user profile and logout functionality
    - Integrated Radix UI components for dropdown and avatar functionality
    - Established constants for navigation items and various options
    - Defined global TypeScript types for form data and stock information
    - Configured Prettier for code formatting
    - Updated package.json with new dependencies for UI components and formatting tools

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,8 @@ import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-5xl text-red-300 underline">Hello World</h1>
-      <Button>Hello World</Button>
+    <div className="flex min-h-screen home-wrapper">
+      Home
     </div>
   )
 }

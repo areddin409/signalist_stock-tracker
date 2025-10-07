c74180f7ed5e5438e7c8c763d6859f2153364e68
Andrew Reddin <areddin409@gmail.com>
2025-10-07 09:42:39 -0500

    fix: remove debug log and adjust button padding in UserDropdown component

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -28,14 +28,12 @@ const UserDropdown = ({
     router.push('/sign-in');
   };
 
-  console.log('user:', user);
-
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button
           variant='ghost'
-          className='flex items-center gap-2 text-gray-4 hover:text-yellow-500 p-2'
+          className='flex items-center gap-3 text-gray-4 hover:text-yellow-500'
         >
           <Avatar className='h-8 w-8'>
             <AvatarImage src='https://github.com/shadcn.png' />
@@ -51,7 +49,7 @@ const UserDropdown = ({
           </div>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className='text-gray-400' sideOffset={10}>
+      <DropdownMenuContent className='text-gray-400'>
         <DropdownMenuLabel>
           <div className='flex relative items-center gap-3 py-2'>
             <Avatar className='h-10 w-10'>

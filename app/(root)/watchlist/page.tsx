dec30d4fbcd29a0838a42bfe7af88dd75a84ad3b
Andrew Reddin <areddin409@gmail.com>
2025-10-07 12:54:44 -0500

    feat: refactor watchlist page to use TradingView widgets and remove WatchlistTable component

--- app/(root)/watchlist/page.tsx
+++ app/(root)/watchlist/page.tsx
@@ -1,55 +1,34 @@
 import { getCurrentUser } from '@/lib/actions/user.actions';
-import { getUserWatchlist } from '@/lib/actions/watchlist.actions';
-import WatchlistTable from '@/components/WatchlistTable';
-import WatchlistAlertWidget from '@/components/WatchlistAlertWidget';
-import { DataTable } from './data-table';
-import { columns } from './columns';
+import {
+  getUserWatchlist,
+  getWatchlistTableData,
+} from '@/lib/actions/watchlist.actions';
+import TradingViewWidget from '@/components/TradingViewWidget';
+import { SYMBOL_INFO_WIDGET_CONFIG } from '@/lib/constants';
 
 export default async function WatchlistPage() {
   const user = await getCurrentUser();
-  const { watchlistSymbols, watchlistCount } = await getUserWatchlist(
-    user.email
-  );
+  const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
+  const { watchlistSymbols } = await getUserWatchlist(user.email);
+  console.log(`🚀 ~ WatchlistPage ~ watchlistSymbols:`, watchlistSymbols);
 
-  const testData = [
-    {
-      symbol: 'AAPL',
-      name: 'Apple Inc.',
-      price: 150.12,
-      company: 'Apple Inc.',
-      change: 2.5,
-      changePercent: 1.67,
-      marketCap: '2.5T',
-      peRatio: 25.4,
-    },
-    {
-      symbol: 'GOOGL',
-      name: 'Alphabet Inc.',
-      price: 2800.45,
-      company: 'Alphabet Inc.',
-      change: -1.2,
-      changePercent: -0.04,
-      marketCap: '1.8T',
-      peRatio: 28.1,
-    },
-    {
-      symbol: 'AMZN',
-      name: 'Amazon.com Inc.',
-      price: 3400.67,
-      company: 'Amazon.com Inc.',
-      change: 5.8,
-      changePercent: 0.17,
-      marketCap: '1.7T',
-      peRatio: 60.5,
-    },
-  ];
+  const { data } = await getWatchlistTableData(watchlistSymbols);
+  console.log(`🚀 ~ WatchlistPage ~ data:`, data);
+
+  if (!data) {
+    return <div>Your watchlist is empty.</div>;
+  }
 
   return (
-    <div>
-      {/* left side watchlist table */}
-      <div className=''>
-        <DataTable columns={columns} data={testData} />
-      </div>
+    <div className='grid grid-cols-1 md:grid-cols-3 gap-6 p-4 md:p-6 lg:p-8 w-full'>
+      {data.map(item => (
+        <TradingViewWidget
+          scriptUrl={`${scriptUrl}symbol-info.js`}
+          config={SYMBOL_INFO_WIDGET_CONFIG(item.symbol)}
+          height={100}
+        />
+      ))}
+
       {/* right side alerts widget */}
       <div></div>
     </div>
